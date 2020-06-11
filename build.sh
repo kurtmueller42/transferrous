df598eae74dcfc4632c31436b0d9954543512ae5
km42 <kurtmueller42@gmail.com>
2020-06-11 14:57:42 -0400

    multithreading yo

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 cargo build
-mv -f ./target/debug/transferrous-server ./testfiles
-mv -f ./target/debug/transferrous-client ./testfiles
+rm -r ./test_directory/
+mkdir ./test_directory
+cp ./testfiles/* ./test_directory/
+mv -f ./target/debug/transferrous-server ./test_directory
+mv -f ./target/debug/transferrous-client ./test_directory
 mv -f ./target/debug/transferrous-client-gui .
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,13 +1,15 @@
 #![allow(unused)]
 use std::io;
-use std::net::{TcpListener, TcpStream};
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
+use tokio::io::BufReader;
+use tokio::prelude::*;
+use tokio::net::{TcpStream, TcpListener};
+use std::error::Error;
 
 const CHUNK_SIZE: usize = 65536;
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+async fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     let mut reader = BufReader::new(stream);
     let mut buf = vec![0; CHUNK_SIZE];
 
@@ -19,7 +21,8 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     let mut file = File::create(format!("{}.out", &outfile_name))?;
 
     loop {
-        match reader.read(buf.as_mut_slice()) {
+        match reader.read(buf.as_mut_slice()).await {
+
           Ok(num_bytes) => {
             if num_bytes > 0 {
                 file.write(&buf[0..num_bytes]);
@@ -33,29 +36,24 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
             break;
           }
         }
+        
       }
     Ok(())
 }
 
-fn listen() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8001")?;
-
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = "127.0.0.1:8001".to_string();
+    let mut listener = TcpListener::bind(&addr).await?;
     println!("Now listening for clients!");
 
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("new client!");
-                handle_client(stream);
-            }
-            Err(e) => { /* connection failed */ }
-        }
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            std::thread::sleep(std::time::Duration::new(10,0));
+            handle_client(socket).await
+        });
+
     }
-    Ok(())
-}
-
-
-fn main() -> io::Result<()> {
-    listen()
 }
